97c6607d675e3607a52d7db6454ed494fcc4ac59
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-12-07 00:18:36 +0200

    markup function

--- src/index.js
+++ src/index.js
@@ -1,38 +1,74 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { common } from './common';
-
-// axios.defaults.headers.common['x-api-key'] = common.API_KEY;
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   form: document.querySelector('#search-form'),
 };
 
-refs.form.addEventListener('submit', getSearchQuery);
+refs.form.addEventListener('submit', onFormSubmit);
 
-async function getSearchQuery(evt) {
+function onFormSubmit(evt) {
   evt.preventDefault();
-  const searchQuery = evt.target.elements.searchQuery.value;
+  getData(evt);
+}
+
+function createMarkup(arrey) {
+  if (!arrey.length) {
+    Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  const markup = arrey.map(el => {
+    return `
+    <div class="photo-card">
+      <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>${el.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>${el.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>${el.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>${el.downloads}
+        </p>
+      </div>
+    </div>`;
+  });
+  return markup;
+}
+
+async function getData(event) {
+  const inputValue = event.target.elements.searchQuery.value;
   try {
-    const images = await fetchImages(searchQuery);
-    console.log(images);
+    const data = await fetchImages(inputValue);
+    console.log(data.hits);
   } catch (error) {
-    console.log(error.message);
+    Notify.failure(error.message);
   }
 }
 
 async function fetchImages(searchQuery) {
-  const searchParams = new URLSearchParams({
-    key: common.API_KEY,
-    q: searchQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  const url = `${common.BASE_URL}?${searchParams}`;
+  const query = {
+    url: common.BASE_URL,
+    method: 'GET',
+    params: {
+      key: common.API_KEY,
+      q: searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
 
-  const response = await axios.get(url);
+  const response = await axios(query);
 
   return response.data;
 }
-
-// ('https://pixabay.com/api/?key=29647131-eec12f6860dbd83baca31563e&q=yellow+flowers&image_type=photo');
